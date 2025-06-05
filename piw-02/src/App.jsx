a56e881cc9019cf116dad53e37e1d7dbf4a338dd
elizadora <elizadoradasilva2003@gmail.com>
2025-06-04 23:58:19 -0300

    movendo codigo do dia 02/06 para uma pasta

--- piw-02/src/App.jsx
+++ piw-02/src/App.jsx
@@ -1,28 +1,9 @@
 import './App.css';
-import ShoppingListCart from './ShoppingListCart';
-import ChangeFace from './ChangeFace';
-import Contador from './Contador';
 
 function App() {
-
-  // criando uma lista de produtos como array de objetos
-  // cada objeto representa um produto com nome, quantidade e se foi comprado ou não
-  let listItemsShopping = [
-    { nome: "Arroz", quantidade: 2, isCompleted: false },
-    { nome: "Feijão", quantidade: 1, isCompleted: false },
-    { nome: "Macarrão", quantidade: 3, isCompleted: false },
-    { nome: "Carne", quantidade: 1, isCompleted: false },
-    { nome: "Frango", quantidade: 2, isCompleted: false },
-    { nome: "Ovos", quantidade: 12, isCompleted: false },
-  ]
-
   return (
     <>
-      {/* chamando o componente ShoppingListCart e passando a lista de produtos como props */}
-      <ShoppingListCart shoppingList={listItemsShopping} />
-      <ChangeFace />
-      <Contador />
-
+      <h1>Turma 02</h1>
     </>
   )
 }
