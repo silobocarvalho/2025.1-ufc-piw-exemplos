eab37951a5d334d19a80585e9476bc19247699fa
Sidartha Carvalho <sidarthacarvalho@MacBook-Air-de-Sidartha.local>
2025-06-08 14:24:45 -0300

    putting things in order

--- piw-02/src/App.jsx
+++ piw-02/src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css';
-import ShoppingListCart from './ShoppingListCart';
-import ChangeFace from './ChangeFace';
-import Contador from './Contador';
+
 import ListaPersonagens from './09-06-2025/RickMorty/ListaPersonagens';
 
 function App() {
   return (
     <>
-      {/* chamando o componente ShoppingListCart e passando a lista de produtos como props */}
-      <ShoppingListCart shoppingList={listItemsShopping} />
-      <ChangeFace />
-      <Contador />
-
       <ListaPersonagens />
 
       <h1>Turma 02</h1>
