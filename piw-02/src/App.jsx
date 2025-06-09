67de79eaf82cdb30ddc27846a225de5d21c3396c
Sidartha Carvalho <sidarthacarvalho@MacBook-Air-de-Sidartha.local>
2025-06-08 22:34:26 -0300

    add rotas com react-router-dom

--- piw-02/src/App.jsx
+++ piw-02/src/App.jsx
@@ -1,16 +1,37 @@
 import ListaDadosAPIMock from './10-06-2025/ListaDadosAPIMock';
 import './App.css';
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import ShoppingListCart from './02-06-2025/ShoppingListCart'
+import Emojies from './03-06-2025/EmojiesComStates/Emojies'
+import Quadradao from './03-06-2025/QuadradaoDeCores/Quadradao'
+import ListaPersonagens from './09-06-2025/RickMorty/ListaPersonagens'
+import NoPage from './NoPage'
 
 
 function App() {
+  
+  let shoppingList = [{id: 1, nome: "Pão", quantidade: 2, isCompleted: true}, {id: 2, nome: "Mortadela", quantidade: 1, isCompleted: false}, ]
+  let listaDeCores = ["#080", "#F00", "#FF0", "#169", "#841"]
   return (
-    <>
-      <h1>Turma 02</h1>
+    <BrowserRouter> 
+      <div>
+        <nav>
+          <a href='/'>ShoppingListCart App | </a>
+          <a href='/emojies'>Emojies com States | </a>
+          <a href='/quadradao'>Quadradão de Cores | </a>
+          <a href='/rickmorty'>Personagens Rick & Morty</a>
+        </nav>
+      </div>        
+        <Routes>
+          <Route path='/' element={<ShoppingListCart shoppingList={shoppingList}/>} />
+          <Route path='/emojies' element={<Emojies />} />
+          <Route path='/quadradao' element={<Quadradao listaDeCores={listaDeCores} />} />
+          <Route path='/rickmorty' element={<ListaPersonagens />} />
 
-    <ListaDadosAPIMock/>
-
-    </>
+          <Route path='*' element={<NoPage />} />
+        </Routes>
+      
+    </BrowserRouter>
   )
 }
 
